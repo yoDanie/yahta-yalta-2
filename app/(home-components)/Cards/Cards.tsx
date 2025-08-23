7302eaa7763b6915fa1b1f35b94f827621097cf6
Daniil Ilyin <d.v.ilyin@tinkoff.ru>
2025-08-23 18:15:07 +0300

    fixed cards for mobile

--- app/(home-components)/Cards/Cards.tsx
+++ app/(home-components)/Cards/Cards.tsx
@@ -6,7 +6,6 @@ import cardImage2 from "./cards-2.jpg"
 import cardImage3 from "./cards-3.jpg"
 import bgImage from "./cards-bg-2.jpg"
 import styles from "./cards.module.scss"
-import polaroidImage from "./polaroid.png"
 import { Button } from "@/components/ui/button"
 import { BookingPopover } from "@/components/BookingPopover/BookingPopover"
 
@@ -17,49 +16,51 @@ const Card = ({ src, alt, caption }: any) => (
   </div>
 )
 
-export const Cards = () => (
-  <Section
-    className={styles.root}
-    title="Наполните жизнь приятными моментами..."
-  >
-    <Image
-      quality={50}
-      className={styles.bgImage}
-      src={bgImage}
-      alt="фоновая картинка"
-    />
-    <div className={styles.cards}>
-      <Card
-        src={cardImage1}
-        alt="Скала парус - Крым, Ялта"
-        caption={`Любуемся скалой „Парус”`}
+export const Cards = () => {
+  return (
+    <Section
+      className={styles.root}
+      title="Наполните жизнь приятными моментами..."
+    >
+      <Image
+        quality={50}
+        className={styles.bgImage}
+        src={bgImage}
+        alt="фоновая картинка"
       />
-      <Card
-        src={cardImage2}
-        alt="Замок Ласточкино Гнездо - Крым, Ялта"
-        caption={`Замок „Ласточкино гнездо”`}
-      />
-      <Card
-        src={cardImage3}
-        alt="Гриль на яхте, готовка рыбы на яхте"
-        caption="Словили рыбку и на гриль!"
-      />
-    </div>
-    <div className={styles.callToAction}>
-      <div className={styles.polaroid}>
-        <div className={styles.photo}>
-          <span className={styles.question}>?</span>
+      <div className={styles.cards}>
+        <Card
+          src={cardImage1}
+          alt="Скала парус - Крым, Ялта"
+          caption={`Любуемся скалой „Парус”`}
+        />
+        <Card
+          src={cardImage2}
+          alt="Замок Ласточкино Гнездо - Крым, Ялта"
+          caption={`Замок „Ласточкино гнездо”`}
+        />
+        <Card
+          src={cardImage3}
+          alt="Гриль на яхте, готовка рыбы на яхте"
+          caption="Словили рыбку и на гриль!"
+        />
+      </div>
+      <div className={styles.callToAction}>
+        <div className={styles.polaroid}>
+          <div className={styles.photo}>
+            <span className={styles.question}>?</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-[50px] sm:w-1/2">
+          <span className="text-center text-xl leading-tight font-light italic sm:text-2xl">
+            Забронируйте морскую прогулку вдоль берега Ялты и добавьте яркие
+            фото в свою коллекцию!
+          </span>
+          <BookingPopover>
+            <Button className="animate-bounce">Забронировать яхту</Button>
+          </BookingPopover>
         </div>
       </div>
-      <div className="flex flex-col items-center gap-[50px] sm:w-1/2">
-        <span className="text-center text-xl leading-tight font-light italic sm:text-2xl">
-          Забронируйте морскую прогулку вдоль берега Ялты и добавьте яркие фото
-          в свою коллекцию!
-        </span>
-        <BookingPopover>
-          <Button className="animate-bounce">Забронировать яхту</Button>
-        </BookingPopover>
-      </div>
-    </div>
-  </Section>
-)
+    </Section>
+  )
+}
